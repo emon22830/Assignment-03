c56cba2df5fa20baf9ff046cc06be155312a06a5
emon22830 <mdemon22830@gmail.com>
2025-08-04 20:32:56 +0800

    first commit

---
+++ Problem1.js
@@ -0,0 +1,7 @@
+// Problem - 01 ( Divide the Assets)
+var area = 800;
+distributedArea = area/2
+
+console.log("Rahim will get =", distributedArea, "area");
+console.log("Karim will get =", distributedArea, "area");
+
