b80f933118e881b5084769121f70faee32c85a9a
emon22830 <mdemon22830@gmail.com>
2025-08-04 21:30:22 +0800

    solved-06

---
+++ Problem6.js
@@ -0,0 +1,7 @@
+// Problem 06 : (Current Salary)
+
+var experiance = 30;
+var startingSalary = 45000;
+
+var CurrentSalary = startingSalary * Math.pow(1.05, experiance);
+console.log(CurrentSalary.toFixed(2));
